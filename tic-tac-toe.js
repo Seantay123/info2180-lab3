d04a91a3895122741f3e10e30e5add4b6f768873
Seantay Johnson <seantayjohnson9@gmail.com>
2025-10-16 15:41:00 -0400

    Added new reset functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -53,3 +53,14 @@ function checkWinner() {
   }
   return false;
 }
+
+const newGameBtn = document.querySelector('.btn');
+newGameBtn.addEventListener('click', () => {
+  squares.forEach(square => {
+    square.textContent = '';
+    square.classList.remove('X', 'O');
+  });
+  document.getElementById('status').textContent = 'Move your mouse over a square and click to play an X or an O.';
+  document.getElementById('status').classList.remove('you-won');
+  currentPlayer = 'X';
+});
